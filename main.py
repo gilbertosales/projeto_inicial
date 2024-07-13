6023327a38bd8baf759dfec723afcd75a763f347
GilbertoSales <ggds@live.com>
2024-07-13 19:58:21 -0300

    Recuperando arquivo main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+print("Jornada de dados")
+
+#Variáveis
+idade = 34
+nome ="Gilberto"
+
+#Print
+print(nome)
+print(idade)
+
+#Soma
+nova_idade = idade + 1
+print(nova_idade)
+
+#Lista
+lista = [1,2,3,4,5]
+
+#Métodos
+
+lista_invertida = lista.reverse()
